18b18332fe2c12bc5dd9fe7ef84aa158e56048a6
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-02-22 15:56:26 -0500

    aplicando constraints a IngresoDiasActivity

--- app/src/main/java/com/example/calculadorahoras/IngresoDiasActivity.kt
+++ app/src/main/java/com/example/calculadorahoras/IngresoDiasActivity.kt
@@ -3,10 +3,10 @@ package com.example.calculadorahoras
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class IngresoHoras : AppCompatActivity() {
+class IngresoHorasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ingreso_horas)
+        setContentView(R.layout.activity_ingreso_dias)
 
     }
 
