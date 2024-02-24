4aad61188dba998f294a8b246c1f8cc2f0a5956c
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-02-23 19:16:07 -0500

    Salto entre primera y segunda ventana

--- app/src/main/java/com/example/calculadorahoras/IngresoDiasActivity.kt
+++ app/src/main/java/com/example/calculadorahoras/IngresoDiasActivity.kt
@@ -1,6 +1,8 @@
 package com.example.calculadorahoras
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,11 +10,15 @@ class IngresoDiasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ingreso_dias)
-        var etDias = findViewById<EditText>(R.id.etCantidadDias)
-        intent.putExtra("dias", etDias.text.toString())
 
 
-
+        val btnIngresar = findViewById<Button>(R.id.btnIngresar)
+        btnIngresar.setOnClickListener {
+            val etDias = findViewById<EditText>(R.id.etCantidadDias)
+            val intent = Intent(this, IngresoHorasExtraActivity::class.java)
+            intent.putExtra("dias", Integer.parseInt(etDias.text.toString()))
+            startActivity(intent)
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/calculadorahoras/IngresoHorasExtraActivity.kt
@@ -0,0 +1,11 @@
+package com.example.calculadorahoras
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class IngresoHorasExtraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingreso_horas_extra)
+    }
+}
